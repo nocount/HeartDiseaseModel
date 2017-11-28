e45f86499f75c23f574aae28baa5c6a28aac1583
Wilson Burchenal <wburchenal@8x8.com>
2017-11-28 10:52:05 -0800

    Adding data and preliminary files

---
+++ logistic_chd_model.py
@@ -0,0 +1,10 @@
+#Wilson Burchenal
+#Stanford tf tutorial Assignment 1
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+# import numpy as np
+import tensorflow as tf
+
+print("Praise the sun")
