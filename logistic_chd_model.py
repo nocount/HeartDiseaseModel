00fc898367e016f3942aee87bae557e25c1b2b7e
Wilson Burchenal <wburchenal@8x8.com>
2017-11-30 14:50:12 -0800

    Working basic model ~95 acc

--- logistic_chd_model.py
+++ logistic_chd_model.py
@@ -10,8 +10,9 @@ import pandas as pd
 import time
 from sklearn.metrics import roc_auc_score
 
-learning_rate = 0.0001
+learning_rate = 0.01
 num_epochs = 100
+batch_size = 10
 
 #Read data in from csv files
 df = pd.read_csv('./data/heart.csv')
@@ -25,25 +26,23 @@ test_size = test.shape[0]
 print(train_size)
 print(test_size)
 
-# print(train.drop('chd', 1))
+
+# test_dict = train.drop('chd',1)[:batch_size]
+# print(test_dict)
 
 #Placeholders
-x_tr = tf.placeholder(dtype=tf.float32, shape=[train_size, 8], name="inputs")
-y_tr = tf.placeholder(dtype=tf.float32, shape=[train_size, 1], name="labels")
-
-x_te = tf.placeholder(dtype=tf.float32, shape=[test_size, 8], name="inputs")
-y_te = tf.placeholder(dtype=tf.float32, shape=[test_size, 1], name="labels")
+x = tf.placeholder(dtype=tf.float32, shape=[batch_size, 8], name="inputs")
+y = tf.placeholder(dtype=tf.float32, shape=[batch_size, 1], name="labels")
 
 # Weights and Bias
 w = tf.Variable(tf.random_normal(shape=[8,1], stddev=0.01), name="weights")
 # b = tf.Variable(tf.zeros(shape=[1,10]), name="bias")
 
 # Model
-logits_tr = tf.matmul(x_tr,w)
-logits_te = tf.matmul(x_te,w)
+logits = tf.matmul(x,w)
 
 #Loss
-loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_tr, labels=y_tr, name="loss"))
+loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=y, name="loss"))
 
 #Gradient
 optimizer=tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
@@ -53,15 +52,18 @@ with tf.Session() as sess:
 	# Train the model
 	start_time=time.time()
 	sess.run(tf.global_variables_initializer())
+	num_batches = int(train_size/batch_size)
 
 	for i in range(num_epochs):
-		X_train = train.drop('chd',1).drop('famhist',1)
-		Y_train = train['chd'].values.reshape(train_size, 1)
+		total_loss = 0
 
-		# print(X_train.shape)
-		# print(Y_train.shape)
+		for _ in range(num_batches):
 
-		_, loss_train = sess.run([optimizer, loss], feed_dict={x_tr: X_train, y_tr: Y_train})
+			X_batch = train.drop('chd',1).drop('famhist',1)[:batch_size]
+			Y_batch = train['chd'].values.reshape(train_size, 1)[:batch_size]
+
+			_, loss_train = sess.run([optimizer, loss], feed_dict={x: X_batch, y: Y_batch})
+			total_loss += loss_train
 
 		print('Loss for epoch {0}: {1}'.format(i, loss_train))
 
@@ -69,16 +71,19 @@ with tf.Session() as sess:
 
 
 	# Test the model
-	preds = tf.nn.sigmoid(logits_tr)
-	correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(y_te, 1))
+	preds = tf.nn.sigmoid(logits)
+	correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(y, 1))
 	accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32))
 
 	total_correct_preds = 0
-	X_test = test.drop('chd',1).drop('famhist',1)
-	Y_test = test['chd'].values.reshape(test_size, 1)
+	
+	num_batches = int(test_size/batch_size)
 
-	accuracy_batch = sess.run([accuracy], feed_dict={x_te: X_test, y_te:Y_test}) 
-	total_correct_preds = sum(accuracy_batch)
+	for i in range(num_batches):
+		X_test = train.drop('chd',1).drop('famhist',1)[:batch_size]
+		Y_test = train['chd'].values.reshape(train_size, 1)[:batch_size]
+		accuracy_batch = sess.run([accuracy], feed_dict={x: X_test, y:Y_test}) 
+		total_correct_preds += sum(accuracy_batch)
 	
 	print('Accuracy {0}'.format(total_correct_preds/test_size))
 
